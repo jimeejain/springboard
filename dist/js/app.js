3ffb50346d5df4f6c3d83b346622cf5128ebef4a
Sujeet Jaiswal <er.sujeetkumarjaiswal@gmail.com>
2016-09-25 22:44:11 +0530

    adding changes related to css and urls

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,19 +2,19 @@
 
 angular.module("springboardApp", ["ngRoute"]).config(["$routeProvider", function ($routeProvider) {
 	$routeProvider.when("/", {
-		templateUrl: "/views/home.html",
+		templateUrl: "./views/home.html",
 		controller: "homeController",
 		controllerAs: "home"
 	}).when("/paths", {
-		templateUrl: "/views/paths.html",
+		templateUrl: "./views/paths.html",
 		controller: "pathListController",
 		controllerAs: "pathList"
 	}).when("/path/:pathId", {
-		templateUrl: "/views/pathView.html",
+		templateUrl: "./views/pathView.html",
 		controller: "pathDetailsController",
 		controllerAs: "path"
 	}).when("/error", {
-		templateUrl: "/views/error.html"
+		templateUrl: "./views/error.html"
 	}).otherwise({
 		redirectTo: "/error"
 	});
